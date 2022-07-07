114094615aa5fb0631b6a2a70fee4c3e55f8de42
Vitalii <i1818i@mail.ru>
2022-07-07 18:30:27 +0300

    1. Added new controller for /ready endpoint 2. Mofified Dockerfile (added curl installing to the image)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM openjdk:8-jdk-alpine
-#за основу взята 8 версия джавы
+#за основу взята 8я версия java
+
+# Install prerequisites
+RUN apk add --update \
+    curl \
+    && rm -rf /var/cache/apk/*
+
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/main/java/com/vetalalien/miniservice/controllers/ReadyController.java
@@ -0,0 +1,42 @@
+package com.vetalalien.miniservice.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+@RestController
+public class ReadyController {
+    public ResponseEntity internalServerError(String msg) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(msg);
+    }
+
+    @GetMapping("/ready")
+    ResponseEntity checkHealth(){
+
+//        return ResponseEntity.ok("All is Ready");
+//        Date d = new Date();
+//        Calendar calendar = GregorianCalendar.getInstance();
+//        calendar.setTime(d);
+//        int seconds = calendar.get(Calendar.SECOND);
+
+        LocalDateTime dt = LocalDateTime.now();
+        int seconds = dt.getSecond();
+
+        ResponseEntity result;
+
+        if (seconds >= 1 & seconds <= 15)
+        {
+            result = internalServerError(Integer.toString(seconds));
+        } else
+            result = ResponseEntity.ok("All is Ready: " + Integer.toString(seconds));
+
+        return result;
+    }
+}
