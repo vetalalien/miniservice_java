660d1ad920d29493a05006764cbba2669ac984a3
tokman <i1818i@mail.ru>
2024-08-04 22:57:03 +0300

    Add some logic

--- src/main/java/com/vetalalien/miniservice/controllers/MainPageController.java
+++ src/main/java/com/vetalalien/miniservice/controllers/MainPageController.java
@@ -1,13 +1,32 @@
 package com.vetalalien.miniservice.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class MainPageController {
+    public ResponseEntity internalServerError(String msg) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(msg);
+    }
     @GetMapping("/")
     ResponseEntity checkHealth(){
-        return ResponseEntity.ok("Hello, otus world!");
+
+        LocalDateTime dt = LocalDateTime.now();
+        int seconds = dt.getSecond();
+
+        ResponseEntity result;
+
+        if (seconds >= 1 & seconds <= 15)
+        {
+            result = internalServerError(Integer.toString(seconds));
+        } else
+            result = ResponseEntity.ok("All is Ready: " + Integer.toString(seconds));
+
+        return result;
+        //return ResponseEntity.ok("Hello, otus world!");
     }
 }
